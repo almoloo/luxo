3a90b8f85dbc6d64e59d98487dbc45693ee368c8
Ali Mousavi <amousavig@icloud.com>
2023-11-27 08:56:18 +0330

    changed type.

--- src/app/utils/getProfile.ts
+++ src/app/utils/getProfile.ts
@@ -25,10 +25,19 @@ export const getProfileData = async (
       }
     );
 
-    const profileData: LSP3Profile | null = (
-      await erc725.fetchData("LSP3Profile")
-    ).value?.LSP3Profile;
-    return profileData;
+    const profileData: any = (await erc725.fetchData("LSP3Profile")).value; //?.LSP3Profile;
+    if (profileData.LSP3Profile) {
+      return profileData as LSP3Profile;
+    } else {
+      return {
+        description: "",
+        name: "",
+        backgroundImage: [],
+        links: [],
+        profileImage: [],
+        tags: [],
+      } as LSP3Profile;
+    }
   } catch (error) {
     return null;
   }
